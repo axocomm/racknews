b2401fd6780e220bd42cf78ab4873253fb6580a2
axocomm <axocomm@gmail.com>
2013-09-10 13:53:59 -0400

    Updated paths and namespace in index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include 'php/Report.class.php';
-use \RT_News\Report as Report;
+include 'php/RackNews/Report.class.php';
+use \RackNews\Report as Report;
 
 $report = new Report();
 $report->build_report();
