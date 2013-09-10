135a5ec9a71d6128ceed8770b4474b3b051e2cc8
Trevor Maglione <tam326@cs.drexel.edu>
2013-09-10 12:41:41 -0400

    Initial commit, very basics right now

---
+++ php/RackNews/Report.class.php
@@ -0,0 +1,70 @@
+<?php
+namespace RackNews;
+
+include 'Util.inc.php';
+
+class Report {
+    const FORMAT_HTML = 1;
+    const FORMAT_CSV = 2;
+    const FORMAT_JSON = 3;
+    const FORMAT_XML = 4;
+    const FORMAT_RAW = 5;
+
+    private $rt_objects;
+    private $report_objects;
+    private $fields;
+    private $types;
+    private $format;
+
+    public function __construct() {
+        $this->rt_objects = array();
+        $this->report_objects = array();
+        $this->fields = array();
+        $this->types = array();
+        $this->format = self::FORMAT_RAW;
+    }
+
+    public function build_report($params = null) {
+        if (!$params) {
+            $params = array(
+                'fields' => $this->fields,
+                'types'  => $this->types,
+                'format' => $this->format
+            );
+        }
+
+        var_dump($params);
+    }
+    
+    public function get_rt_objects() {
+        return $this->rt_objects;
+    }
+
+    public function get_report_objects() {
+        return $this->report_objects;
+    }
+
+    public function get_fields() {
+        return $this->fields;
+    }
+
+    public function get_types() {
+        return $this->types;
+    }
+
+    public function get_format() {
+        return $this->format;
+    }
+
+    public function set_fields($fields) {
+        $this->fields = $fields;
+    }
+
+    public function set_types($types) {
+        $this->types = $types;
+    }
+
+    public function set_format($format) {
+        $this->format = $format;
+    }
+}
---
+++ php/RackNews/Util.inc.php
@@ -0,0 +1,27 @@
+<?php
+$script_mode = true;
+include '../inc/init.php';
+
+#if (!isset($racktables_rootdir)) {
+#    $racktables_rootdir = substr(dirname(__FILE__), 0, strrpos(dirname(__FILE__), '/'));
+#    if ($script_mode or authenticate_rt()) {
+#        try {
+#            include("$racktables_rootdir/inc/init.php");
+#        } catch (Exception $e) {
+#            if ($e->getCode() == RackTablesError::NOT_AUTHENTICATED) {
+#                authenticate_rt(1);
+#            }
+#        }
+#    }
+#}
+
+function authenticate_rt($f = 0) {
+    if ($f or !isset($_SERVER['PHP_AUTH_USER']) or !strlen($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="RackTables"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'You must enter a username and password.';
+        exit;
+    } else {
+        return 1;
+    }
+}
