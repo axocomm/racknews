9c2ee27e9f551c799dcbec1fdec71e5e6a19046d
axocomm <axocomm@gmail.com>
2013-09-11 12:00:11 -0400

    Updated index

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 include 'init.php';
-
 use \RackNews\Report as Report;
+use \RackNews\RTObject as RTObject;
 
-$report = new Report();
-$report->build_report();
+$report = new Report(RTObject::get_objects());
+$params = array(
+    'fields' => array('name', 'FQDN'),
+    'format' => Report::FORMAT_CSV
+);
+
+$report->set_params($params);
+$report->build();
+$report->display();
