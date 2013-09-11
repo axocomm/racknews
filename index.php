561cc0f174ab2ac679c7085b6ea2421d61f94a53
axocomm <axocomm@gmail.com>
2013-09-11 11:45:45 -0400

    index.php for testing reports and objects

--- index.php
+++ index.php
@@ -4,4 +4,11 @@ use \RackNews\Report as Report;
 use \RackNews\RTObject as RTObject;
 
 $report = new Report(RTObject::get_objects());
-print_r($report->get_objects());
+$params = array(
+    'fields' => array('name, FQDN'),
+    'format' => Report::FORMAT_CSV
+);
+
+$report->set_params($params);
+$report->build();
+
