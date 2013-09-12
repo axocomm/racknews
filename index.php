1eac393665ac7e261f0df574e833dc80e0f5478c
axocomm <axocomm@gmail.com>
2013-09-12 15:01:52 -0400

    Basics of view

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<form action="report.php" method="get">
-    <input type="hidden" name="fields" value="name,id,FQDN,objtype_id,etags">
-    <input type="hidden" name="types" value="server">
-    <input type="hidden" name="format" value="json">
-    <input type="submit" value="Test">
-</form>
+<?php include 'php/view/header.php'; ?>
+            <form action="report.php" method="get">
+                <input type="hidden" name="fields" value="name,id,FQDN,objtype_id,etags">
+                <input type="hidden" name="types" value="server">
+                <input type="hidden" name="format" value="json">
+                <input type="submit" value="Test">
+            </form>
+<?php include 'php/view/footer.php'; ?>
---
+++ php/view/footer.php
@@ -0,0 +1,5 @@
+        <footer>
+            Copyright 2013, Foobar Inc.
+        </footer>
+    </body>
+</html>
