f06d6d7bf6723b8e9f2599337aa6f6810e91f240
axocomm <axocomm@gmail.com>
2013-09-12 09:27:02 -0400

    Added get_addrs and get_allocs

--- php/RackNews/Util.class.php
+++ php/RackNews/Util.class.php
@@ -2,6 +2,40 @@
 namespace RackNews;
 
 class Util {
+    public static function get_addrs() {
+        $rt_nets = scanRealmByText('ipv4net');
+
+        $addrs = array();
+        foreach ($rt_nets as $i => $net) {
+            $info = spotEntity('ipv4net', $i);
+            loadIPv4AddrList($info);
+
+            foreach ($info['addrlist'] as $addr) {
+                $addrs[] = array(
+                    'object_name' => $addr['name'],
+                    'ip' => $addr['ip']
+                );
+            }
+        }
+
+        return $addrs;
+    }
+
+    public static function get_allocs($objects) {
+        $allocs = array();
+        foreach ($objects as $object) {
+            $ipv4 = $object['ipv4'];
+            foreach ($ipv4 as $alloc) {
+                $allocs[$alloc['addrinfo']['ip']] = array(
+                    'object' => $object['name'],
+                    'interface' => $alloc['osif']
+                );
+            }
+        }
+
+        return $allocs;
+    }
+
     public static function multi_implode($pieces, $glue) {
         $out = '';
 
