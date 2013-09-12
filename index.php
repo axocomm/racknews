85319488c38157f88c2133991f02e90e49d00e9d
axocomm <axocomm@gmail.com>
2013-09-12 15:18:59 -0400

    Added basic form styling

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php include 'php/view/header.php'; ?>
-            <form action="report.php" method="get">
-                <input type="hidden" name="fields" value="name,id,FQDN,objtype_id,etags">
-                <input type="hidden" name="types" value="server">
-                <input type="hidden" name="format" value="json">
-                <input type="submit" value="Test">
+            <form id="report-form" action="report.php" method="get">
+                <fieldset>
+                    <legend>Report Generation</legend>
+                    <div class="control-group">
+                        <label class="control-label" for="fields">Fields</label>
+                        <div class="controls">
+                            <input type="text" name="fields" placeholder="name,id,FQDN,objtype_id,etags">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="types">Types</label>
+                        <div class="controls">
+                            <input type="text" name="types" placeholder="server">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="format">Format</label>
+                        <div class="controls">
+                            <input type="text" name="format" placeholder="json">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="controls">
+                            <input class="btn btn-primary" type="submit" value="Test">
+                        </div>
+                    </div>
+                </fieldset>
             </form>
 <?php include 'php/view/footer.php'; ?>
