7f47fcc21b69b5bc5607a36bc50e73d9ea490fd0
axocomm <axocomm@gmail.com>
2013-09-12 13:52:59 -0400

    Uncommented authentication in init.php

--- init.php
+++ init.php
@@ -3,21 +3,18 @@ include 'php/RackNews/Report.class.php';
 include 'php/RackNews/ObjectUtils.class.php';
 include 'php/RackNews/Util.class.php';
 
-$script_mode = true;
-include '../inc/init.php';
-
-#if (!isset($racktables_rootdir)) {
-#    $racktables_rootdir = substr(dirname(__FILE__), 0, strrpos(dirname(__FILE__), '/'));
-#    if ($script_mode or authenticate_rt()) {
-#        try {
-#            include("$racktables_rootdir/inc/init.php");
-#        } catch (Exception $e) {
-#            if ($e->getCode() == RackTablesError::NOT_AUTHENTICATED) {
-#                authenticate_rt(1);
-#            }
-#        }
-#    }
-#}
+if (!isset($racktables_rootdir)) {
+    $racktables_rootdir = substr(dirname(__FILE__), 0, strrpos(dirname(__FILE__), '/'));
+    if ($script_mode or authenticate_rt()) {
+        try {
+            include("$racktables_rootdir/inc/init.php");
+        } catch (Exception $e) {
+            if ($e->getCode() == RackTablesError::NOT_AUTHENTICATED) {
+                authenticate_rt(1);
+            }
+        }
+    }
+}
 
 function authenticate_rt($f = 0) {
     if ($f or !isset($_SERVER['PHP_AUTH_USER']) or !strlen($_SERVER['PHP_AUTH_USER'])) {
