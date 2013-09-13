122931d170e83c12c5a38a875d5c9f89f0e0dbba
axocomm <axocomm@gmail.com>
2013-09-12 23:04:09 -0400

    Added jQuery and bootstrap.min.js

--- resources/template-parts/footer.php
+++ resources/template-parts/footer.php
@@ -2,5 +2,7 @@
                 Copyright 2013, Foobar Inc.
             </footer>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="resources/js/bootstrap.min.js"></script>
     </body>
 </html>
