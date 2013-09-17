9fc86d2903df1c48c0f0bf1635f1c4959ddf3c9d
axocomm <axocomm@gmail.com>
2013-09-17 11:11:12 -0400

    Moved create form JavaScript to resources/js/create-form.js

--- index.php
+++ index.php
@@ -113,48 +113,5 @@
                     <a href="#" class="btn" data-dismiss="modal">Close</a>
                 </div>
             </div>
-            <script>
-                function getFormData() {
-                    var query = [];
-                    var formData = $('#report-form input:not([type="checkbox"])').serializeArray();
-                    for (var i in formData) {
-                        if (formData[i].value.length) {
-                            query.push(formData[i].name + '=' + formData[i].value);
-                        }
-                    } 
-
-                    var fields = [];
-                    $('#report-form').find('input[name="fields[]"]:checked').each(function() {
-                        fields.push(this.value);
-                    });
-
-                    if (fields.length) {
-                        query.push('fields=' + fields.join(','));
-                    }
-
-                    var has = [];
-                    $('#report-form').find('input[name="has[]"]:checked').each(function() {
-                        has.push(this.value);
-                    });
-
-                    if (has.length) {
-                        query.push('matching=' + has.join(','));
-                    }
-
-                    $('#report-form').find('select').each(function() {
-                        query.push(this.name + '=' + this.value);
-                    });
-
-                    var queryString = query.join('&');
-
-                    $('#query-string').text(queryString);
-                    $('#query-string-modal').modal();
-                }
-
-                function clearCheckboxes(group) {
-                    $('#fields-table td.' + group + '-check input:checkbox').each(function() {
-                        $(this).attr('checked', false);
-                    });
-                }
-            </script>
+            <script src="resources/js/create-form.js"></script>
 <?php include 'resources/template-parts/footer.php'; ?>
