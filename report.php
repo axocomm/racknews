50453f44cc2902860ba521662baa8249bbbc4cec
axocomm <axocomm@gmail.com>
2013-09-17 11:18:09 -0400

    Added command line functionality to report.php

--- report.php
+++ report.php
@@ -1,10 +1,25 @@
 <?php
+if (php_sapi_name() === 'cli') {
+    $script_mode = TRUE;
+}
+
 include 'init.php';
 use \RackNews\Report as Report;
 use \RackNews\ObjectUtils as ObjectUtils;
 
 $report = new Report(ObjectUtils::get_objects());
-$params = $_REQUEST;
+if (php_sapi_name() === 'cli') {
+    $script_mode = 1;
+    $longopts = array(
+        'report:', 'has:', 'types:', 'names:',
+        'id:', 'log:', 'comment:', 'fields:',
+        'matching:', 'format:'
+    );
+
+    $params = getopt('', $longopts);
+} else {
+    $params = $_REQUEST;
+}
 
 $report->set_params($params);
 try {
