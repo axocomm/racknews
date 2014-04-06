190345ec9bb7e3002570176b1446560e5a23e594
axocomm <axocomm@gmail.com>
2014-04-06 03:11:21 -0400

    Add viewport meta tag

--- resources/template-parts/header.php
+++ resources/template-parts/header.php
@@ -3,6 +3,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="<?php echo $rd; ?>/resources/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo $rd; ?>/resources/css/bootstrap-responsive.min.css">
         <link rel="stylesheet" href="<?php echo $rd; ?>/resources/css/styles.css">
