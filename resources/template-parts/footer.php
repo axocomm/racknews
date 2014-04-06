c18a7c9ed43d909f35a35f0306c2d9ba4d7ee71e
axocomm <axocomm@gmail.com>
2014-04-06 02:59:44 -0400

    Move footer outside #main and style

--- resources/template-parts/footer.php
+++ resources/template-parts/footer.php
@@ -1,7 +1,7 @@
-            <footer>
-                Copyright &copy; <?php echo date('Y'); ?>, <a href="#">Foobar</a>
-            </footer>
         </div>
+        <footer>
+            Copyright &copy; <?php echo date('Y'); ?>, <a href="#">Foobar</a>
+        </footer>
         <script src="<?php echo $rd; ?>/resources/js/bootstrap.min.js"></script>
     </body>
 </html>
